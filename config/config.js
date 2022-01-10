21db3de7d6aebe5640e6cd3856a98530f84513fb
panuhen <hentunen@gmail.com>
2022-01-10 08:54:10 +0200

    v1.1

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 const config = {
-    app_name: 'Cool Rarity',
+    app_name: 'Velas Dogs Rarity App',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: 'collection.json',
-    collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
-    collection_name: '10k One Day Punks',
-    collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
-    collection_id_from: 0,
+    collection_contract_address: '0x8D95879dEF5fB1b8EAFce52cD5903ca68B181145',
+    collection_name: 'Velas Dogs',
+    collection_description: 'Velas Dogs is a collection of unique NFTs that live on the Velas blockchain | velasdogs.com',
+    collection_id_from: 1,
     ignore_traits: ['date'], 
     sqlite_file_name: 'database.sqlite',
-    ga: 'G-BW69Z04YTP',
-    main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
-    item_path_name: 'punk',
+    ga: '',
+    main_og_image: '/images/og.png',
+    item_path_name: 'dog',
     page_item_num: 60,
     content_image_is_video: false,
-    content_image_frame: 'circle', // circle, rectangle
+    content_image_frame: 'rectangle', // circle, rectangle
     use_wallet: false
 };
 
